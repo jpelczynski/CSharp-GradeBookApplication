f1d32c42cc2abf2fc3ab016832be6e85e696965a
Jason Pelczynski <jpelczynski@ehs.com>
2021-01-28 06:12:29 -0500

    override letter grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,30 @@ namespace GradeBook.GradeBooks
             this.Name = name;
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                if (averageGrade >= 90)
+                    return 'A';
+                else if (averageGrade >= 80)
+                    return 'B';
+                else if (averageGrade >= 70)
+                    return 'C';
+                else if (averageGrade >= 60)
+                    return 'D';
+                else
+                    return 'F';
+            }
+
+
+            
+            return base.GetLetterGrade(averageGrade);
+        }
     }
 }
